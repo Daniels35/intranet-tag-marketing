0104ea61455fe6c270cf022c11c4722114033585
Daniel Diaz <danielstiven35@gmail.com>
2024-02-29 18:36:55 -0500

    Items administración en modal

--- client/puntos/src/pages/Admin/Admin.jsx
+++ client/puntos/src/pages/Admin/Admin.jsx
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import UsersAdmin from '../../components/UsersAdmin/UsersAdmin';
 import PointsItemsAdmin from '../../components/PointsItemsAdmin/PointsItemsAdmin';
 import RedeemableItemsAdmin from '../../components/RedeemableItemsAdmin/RedeemableItemsAdmin';
-
-
+import Modal from '../../components/Modal/Modal';
+import './Admin.css'
 
 const Admin = () => {
+
+
+  const [usersOpen, setUser] = useState(false);
+  const [pointsOpen, setPoints] = useState(false);
+  const [redeemableItemOpen, setredeemableItem] = useState(false);
+
   return (
+    <>
     <div className="admin-container">
       <h1>Panel de Administración</h1>
-      <UsersAdmin />
-      <PointsItemsAdmin />
-      <RedeemableItemsAdmin />
+
+      <h2 onClick={(e) => { e.stopPropagation(); setUser(true);}}>Administración de Usuarios</h2>
+      <h2 onClick={(e) => { e.stopPropagation(); setPoints(true);}}>Administración de Items de Puntos</h2>
+      <h2 onClick={(e) => { e.stopPropagation(); setredeemableItem(true);}}>Administración de Items Canjeables</h2>
+
     </div>
+      
+      {/* Modales */}
+      <Modal isVisible={usersOpen} onClose={() => setUser(false)}>
+       <UsersAdmin />
+      </Modal>
+
+      <Modal isVisible={pointsOpen} onClose={() => setPoints(false)}>
+      <PointsItemsAdmin />
+      </Modal>
+
+      <Modal isVisible={redeemableItemOpen} onClose={() => setredeemableItem(false)}>
+      <RedeemableItemsAdmin />
+      </Modal>
+    </>
   );
 };
 
