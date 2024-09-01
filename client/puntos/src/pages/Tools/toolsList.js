d4c45f5bfcc85deaa8829816679715e664489047
Daniel Diaz <danielstiven35@gmail.com>
2024-08-31 21:52:17 -0500

    Nuevos proyectos listados

--- client/puntos/src/pages/Tools/toolsList.js
+++ client/puntos/src/pages/Tools/toolsList.js
@@ -1,9 +1,11 @@
 const tools = [
     { name: "Generador de códigos QR", component: "QRGenerator" },
+    { name: "Recordatorios por whatsapp y email tareas", component: null },
     { name: "Chatbot parrillas redes", component: null },
     { name: "Chatbot pauta", component: null },
     { name: "Chatbot blogs", component: null },
-    { name: "Cotización Automatica", component: null }
+    { name: "Cotización Automatica", component: null },
+    { name: "Transcribir videos  - audios ( ingles, español)", component: null }
 ];
 
 export default tools;
