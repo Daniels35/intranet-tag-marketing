e9801e9623a2a95f9227dcb743068cde51564ab8
Daniel Diaz <danielstiven35@gmail.com>
2024-09-29 18:13:42 -0500

    Estilos botones editar y eliminar listos

--- client/puntos/src/components/PointsItemsList/PointsItemsList.jsx
+++ client/puntos/src/components/PointsItemsList/PointsItemsList.jsx
@@ -1,15 +1,29 @@
 // PointsItemsList.js
 import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import './pointsItemActionsAdmin.css'
 
 const PointsItemsList = ({ pointsItems, editPointsItem, deletePointsItem }) => (
   <div>
     <h2>Lista de Items de Puntos</h2>
+    <div className='points-item-actions-admin'>
+             <span>
+                <FaEdit className='points-item-icon' /> Editar
+              </span> 
+              <span>
+                <FaTrash className='points-item-icon' /> Eliminar
+              </span>
+      </div>
     <ul>
       {pointsItems.map(item => (
         <li key={item.id}>
           {item.name} - {item.points} puntos
-          <button onClick={() => editPointsItem(item)}>Editar</button>
-          <button onClick={() => deletePointsItem(item.id)}>Eliminar</button>
+            <button className='points-item-btn' onClick={() => editPointsItem(item)}>
+              <FaEdit className='points-item-icon' />
+            </button>
+            <button className='points-item-btn' onClick={() => deletePointsItem(item.id)}>
+              <FaTrash className='points-item-icon' />
+            </button>
         </li>
       ))}
     </ul>
--- client/puntos/src/components/RedeemableItemsList/RedeemableItemsList.jsx
+++ client/puntos/src/components/RedeemableItemsList/RedeemableItemsList.jsx
@@ -1,15 +1,29 @@
 // RedeemableItemsList.js
 import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+import '../PointsItemsList/pointsItemActionsAdmin.css'
 
 const RedeemableItemsList = ({ redeemableItems, editRedeemableItem, deleteRedeemableItem }) => (
   <div>
     <h2>Lista de Items Canjeables</h2>
+    <div className='points-item-actions-admin'>
+             <span>
+                <FaEdit className='points-item-icon' /> Editar
+              </span> 
+              <span>
+                <FaTrash className='points-item-icon' /> Eliminar
+              </span>
+      </div>
     <ul>
       {redeemableItems.map(item => (
         <li key={item.id}>
-          {item.name} - {item.costInPoints} puntos - Stock: {item.stock}
-          <button onClick={() => editRedeemableItem(item)}>Editar</button>
-          <button onClick={() => deleteRedeemableItem(item.id)}>Eliminar</button>
+          {item.name} - {item.costInPoints}
+          <button className='points-item-btn' onClick={() => editRedeemableItem(item)}>
+            <FaEdit className='points-item-icon' />
+          </button>
+          <button className='points-item-btn' onClick={() => deleteRedeemableItem(item.id)}>
+            <FaTrash className='points-item-icon' />
+          </button>
         </li>
       ))}
     </ul>
