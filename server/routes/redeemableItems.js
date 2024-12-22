83b5d9faa29e16dcda1b53b8e8ea6b1181f89380
Daniel Diaz <danielstiven35@gmail.com>
2024-12-21 21:32:43 -0500

    Manejo de imágenes por parte del backend con sus repectivos manejo de errores y eliminación de las imágenes en caso de borrar el item

--- server/routes/redeemableItems.js
+++ server/routes/redeemableItems.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const multer = require('multer');
 const router = express.Router();
 const redeemableItemsController = require('../controllers/redeemableItemsController');
 
+// Configurar multer para manejar la subida de archivos
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+// Ruta para obtener todos los items
 router.get('/redeemableItems', redeemableItemsController.getAllItems);
+
+// Ruta para obtener un item por su ID
 router.get('/redeemableItems/:id', redeemableItemsController.getItemById);
-router.post('/redeemableItems', redeemableItemsController.createItem);
-router.put('/redeemableItems/:id', redeemableItemsController.updateItem);
+
+// Ruta para crear un nuevo item (incluye subida de archivo)
+router.post('/redeemableItems', upload.single('image'), redeemableItemsController.createItem);
+
+// Ruta para actualizar un item por su ID
+router.put('/redeemableItems/:id', upload.single('image'), redeemableItemsController.updateItem);
+
+// Ruta para eliminar un item por su ID
 router.delete('/redeemableItems/:id', redeemableItemsController.deleteItem);
 
 module.exports = router;
