91543908d2f0fe79eb165510d0cf432de590aa55
Daniel Diaz <danielstiven35@gmail.com>
2025-09-15 11:05:14 -0500

    Esconder demás herramientas

--- client/puntos/src/pages/Tools/toolsList.js
+++ client/puntos/src/pages/Tools/toolsList.js
@@ -1,12 +1,12 @@
 const tools = [
     { name: "Generador de códigos QR", component: "QRGenerator" },
-    { name: "Recordatorios por whatsapp y email tareas", component: null },
-    { name: "Recordatorios por WhatsApp y correo electrónico para clientes: pronto pago", component: null },
-    { name: "Chatbot parrillas redes", component: null },
-    { name: "Chatbot pauta", component: null },
-    { name: "Chatbot blogs", component: null },
-    { name: "Cotización Automatica", component: null },
-    { name: "Transcribir videos  - audios ( ingles, español)", component: null }
+    // { name: "Recordatorios por whatsapp y email tareas", component: null },
+    // { name: "Recordatorios por WhatsApp y correo electrónico para clientes: pronto pago", component: null },
+    // { name: "Chatbot parrillas redes", component: null },
+    // { name: "Chatbot pauta", component: null },
+    // { name: "Chatbot blogs", component: null },
+    // { name: "Cotización Automatica", component: null },
+    // { name: "Transcribir videos  - audios ( ingles, español)", component: null }
 ];
 
 export default tools;
