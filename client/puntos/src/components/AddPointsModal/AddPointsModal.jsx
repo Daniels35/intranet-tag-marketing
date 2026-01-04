3d12efef0691d4d09015e27ce6c923acc8adec72
Daniel Diaz <danielstiven35@gmail.com>
2026-01-04 18:27:49 -0500

    Eliminación importación que generaba problema

--- client/puntos/src/components/AddPointsModal/AddPointsModal.jsx
+++ client/puntos/src/components/AddPointsModal/AddPointsModal.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchUserInfo } from '../../redux/userSlice';
-import '../AddPointsToList/ModalAddPointsToList.css';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
